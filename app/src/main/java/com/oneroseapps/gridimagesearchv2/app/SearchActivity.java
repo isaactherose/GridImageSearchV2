7b7868b859f953b424eb6a10bd01b3ee4f832d7e
isaac rose <isaacfrose@gmail.com>
2014-06-18 20:54:40 -0700

    imageResults class working

--- app/src/main/java/com/oneroseapps/gridimagesearchv2/app/SearchActivity.java
+++ app/src/main/java/com/oneroseapps/gridimagesearchv2/app/SearchActivity.java
@@ -1,5 +1,6 @@
 package com.oneroseapps.gridimagesearchv2.app;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -9,6 +10,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 //test 1
 //test 2
 
@@ -17,6 +27,8 @@ public class SearchActivity extends ActionBarActivity {
     EditText etQuery;
     Button btnSearch;
     GridView  gvResults;
+    //create the array. this is the constructor and you can put any type into the array
+    ArrayList<imageResults> imageResults1 = new ArrayList<imageResults>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +42,31 @@ public class SearchActivity extends ActionBarActivity {
 
     }
     public void onImageSearch(View V){
+        //get text from editText and show a toast
         String query = etQuery.getText().toString();
         Toast.makeText(this, "Searching for " + query, Toast.LENGTH_LONG).show();
+     // send an async request to get stuff from the API
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get("https://ajax.googleapis.com/ajax/services/search/images?rsz=&&" + "start=" + 0 + "&v=1.0&q=" + Uri.encode(query),
+                new JsonHttpResponseHandler(){
+                    @Override
+                    //what to do with the response you get from the google api
+                public void onSuccess(JSONObject response){
+                        // initializes the jsonarray
+                    JSONArray imageJsonResults = null;
+                        try{
+                            //this will put the json response  into the array
+                            imageJsonResults = response.getJSONObject("responseData").getJSONArray("results");
+                            //this will clear the array
+                            imageResults1.clear();
+                            //this will add the results types of the json object to imageResults1 array
+                            imageResults1.addAll(imageResults.fromJSONArray(imageJsonResults));
+                        } catch (JSONException e){
+                            e.printStackTrace();
+                        }
+
+                }
+                });
     }
 
 
--- app/src/main/java/com/oneroseapps/gridimagesearchv2/app/imageResults.java
+++ app/src/main/java/com/oneroseapps/gridimagesearchv2/app/imageResults.java
@@ -1,7 +1,10 @@
 package com.oneroseapps.gridimagesearchv2.app;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class imageResults {
 
@@ -19,6 +22,24 @@ public class imageResults {
         return this.thumbUrl;
     }
 
+    //this function creates an arrayList and then fills that arraylist with the images of the JSONArray
+    public static ArrayList<imageResults> fromJSONArray(
+            JSONArray array){
+        //creates the arrayList called results
+        ArrayList<imageResults> results = new ArrayList<imageResults>();
+        //fills up the results array
+        for(int x = 0; x <array.length(); x++){
+         try{
+             results.add(new imageResults(array.getJSONObject(x)));
+         } catch(JSONException e){
+             e.printStackTrace();
+         }
+        }
+        //produces the array
+        return results;
+    }
+
+    //the constructor for the class that gets the object and gets the string
     public imageResults(JSONObject json){
         try{
             this.fullUrl = json.getString("url");
@@ -32,3 +53,4 @@ public class imageResults {
 
 }
 //class comment test
+//comment 2
